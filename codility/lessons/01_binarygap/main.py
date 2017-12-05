4206a87e4b4d9e8b064ae03d1f565c8a8c172386
Paul <paul@cyence.net>
2017-12-04 18:12:19 -0800

    add comments

--- codility/lessons/01_binarygap/main.py
+++ codility/lessons/01_binarygap/main.py
@@ -1,16 +1,30 @@
-#!/user/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # https://codility.com/programmers/lessons/1-iterations/binary_gap/
 
+# Constraints:
+# Time O(log(N))
+# Space O(1)
+
 def solution(N):
+    # first convert integer to binary string
+    # iterate through string while tracking:
+    #     first initial 1 (one_track)
+    #     subsequent closing 1 (i == '1')
+    #     number of 0 within 1s (gap_counter)
+    #     largest number of 0 within 1s (max_gap)
     int_to_bin = '{0:b}'.format(N)
     max_gap = 0
     gap_counter = 0
     one_track = 0
     for i in int_to_bin:
+        # count 0 if opening 1 occurred
         if one_track == 1 and i == '0':
             gap_counter += 1
+        # trackings first 1 occurence
         if i == '1' and one_track == 0:
             one_track = 1
+        # finds largest gap_counter
         elif i == '1' and one_track == 1:
             max_gap = gap_counter if gap_counter > max_gap else max_gap
             gap_counter = 0
